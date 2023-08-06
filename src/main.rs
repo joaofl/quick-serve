4806175249420397351231211094c41960d60c98
João Loureiro <joaofl@gmail.com>
2023-08-07 00:19:10 +0200

    Get FTP server startup working from button press

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use tokio::try_join;
-use std::path::PathBuf;
-use log::{error, info};
+// Only mind errors for now
+// #![allow(warnings)]
+
+use std::{path::PathBuf};
+use log::{debug};
 
 // use chrono::NaiveDate;
 // use slint::SharedString;
@@ -11,40 +13,21 @@ mod servers {
     pub mod ftp;
 }
 
-// Below the code for command line parser
-// #[derive(Parser)]
-// #[command(author, version, about, long_about = None)]
-// #[command(next_line_help = true)]
-// struct Cli {
-//     #[arg(long)]
-//     path: String,
-//     port: u32,
-// }
 
-
-fn main() {
+#[tokio::main]
+async fn main() {
     ::std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
     debug!("Starting my shapp");
 
-
     let ui = AnyServeUI::new().unwrap();
 
-    let fut_ftp = servers::ftp::start_ftp_server(PathBuf::from("/tmp/"), 21);
-    let result = try_join!(fut_ftp);
-
-    match result {
-        Ok(s) => info!("result is...\n{:?}", s),
-        Err(e) => error!("There was an error: {:?}", e)
-    };
-
     ui.on_start_ftp_server(move || {
-        let ftp_server = servers::ftp::FTPServer::new(PathBuf::from("/tmp/"), 2121);
-        // ftp_fut = ftp_server.start();
-        // let ftp_join = tokio::spawn(ftp_fut);
-        // let result = try_join!(ftp_server.start());
-        debug!("Starting.....")
+        tokio::spawn(async move {
+            let _ = servers::ftp::start_ftp_server(PathBuf::from("/tmp/"), 2121).await;
+            // let result = fut.await.unwrap();
+        });
     });
 
     ui.run().unwrap();
--- src/servers/ftp.rs
+++ src/servers/ftp.rs
@@ -1,26 +1,26 @@
-use libunftp::ServerError;
+use libunftp::{ServerError};
+use unftp_sbe_fs::{ServerExt};
 use log::info;
-use std::path::{PathBuf};
-use unftp_sbe_fs::ServerExt;
+use std::path::PathBuf;
 
-pub struct FTPServer {
-    // server: libunftp::Server,
-    path: PathBuf,
-    port: u32,
+// pub struct FTPServer {
+    // server: Server<Filesystem, DefaultUser>,
+    // address: String,
+// }
+
+// impl FTPServer {
+
+// pub async fn start_ftp_server(path: PathBuf, port: u32) -> impl Future<Output = Result<(), ServerError>>  {
+pub async fn start_ftp_server(path: PathBuf, port: u32) -> Result<(), ServerError>  {
+    let server = libunftp::Server::with_fs(path.clone()).passive_ports(50000..65535);
+    let address = format!("127.0.0.1:{}", port);
+
+    info!("Starting the FTP server at {}", address);
+    server.listen(address.clone()).await
 }
 
-impl FTPServer {
-
-    pub fn new(path: PathBuf, port: u32) -> Self {
-        info!("Starting the FTP server");
-        return FTPServer { path, port };
-    }
-
-    pub async fn start(&self) -> Result<(), ServerError> {
-        // let ftp_home = std::env::temp_dir();
-        let server = libunftp::Server::with_fs(self.path.clone()).passive_ports(50000..65535);
-        let address = format!("127.0.0.1:{}", self.port);
-
-        server.listen(address).await
-    }
-}
+    // pub async fn start(&self) {
+    //     info!("Starting the FTP server at {}", self.address);
+    //     // self.server.listen(self.address.clone()).await.unwrap();
+    // }
+// }
