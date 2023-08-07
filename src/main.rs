bd124d738619e3d5cc12598079ea06b39d83814b
João Loureiro <joaofl@gmail.com>
2023-08-08 00:50:52 +0200

    Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 // Only mind errors for now
 // #![allow(warnings)]
 
-use std::{path::PathBuf};
-use log::{debug};
-
-// use chrono::NaiveDate;
-// use slint::SharedString;
+use std::path::PathBuf;
+use std::sync::Arc;
+use log::{info, debug};
 
 slint::slint!(import { AnyServeUI } from "src/ui.slint";);
 
-mod servers {
-    pub mod ftp;
-}
+mod servers { pub mod ftp; }
+use crate::servers::ftp::FTPServer;
 
 
-#[tokio::main]
-async fn main() {
+fn main() {
     ::std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
@@ -23,11 +19,16 @@ async fn main() {
 
     let ui = AnyServeUI::new().unwrap();
 
+    let shared_ftp_server = Arc::new(FTPServer::new(PathBuf::from("/tmp/"), 2121));
+    let ftp_server_c1 = Arc::clone(&shared_ftp_server);
+    let ftp_server_c2 = Arc::clone(&shared_ftp_server);
+
     ui.on_start_ftp_server(move || {
-        tokio::spawn(async move {
-            let _ = servers::ftp::start_ftp_server(PathBuf::from("/tmp/"), 2121).await;
-            // let result = fut.await.unwrap();
-        });
+        ftp_server_c1.start();
+    });
+    
+    ui.on_stop_ftp_server(move || {
+        ftp_server_c2.stop();
     });
 
     ui.run().unwrap();
