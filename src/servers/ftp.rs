97135375e67af9889ab79d8a6dc5e2f223e3a978
João Loureiro <joaofl@gmail.com>
2023-08-09 00:53:25 +0200

    Get ftp nearly working

--- src/servers/ftp.rs
+++ src/servers/ftp.rs
@@ -1,26 +1,94 @@
-use libunftp::{ServerError};
-use unftp_sbe_fs::{ServerExt};
-use log::info;
+use libunftp::{Server, ServerError};
+use libunftp::auth::DefaultUser;
+use unftp_sbe_fs::{Filesystem, ServerExt};
+use log::{debug, info};
 use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
 
-// pub struct FTPServer {
-    // server: Server<Filesystem, DefaultUser>,
-    // address: String,
-// }
+use tokio::sync::broadcast;
 
-// impl FTPServer {
+use std::thread;
+use std::time::Duration;
 
-// pub async fn start_ftp_server(path: PathBuf, port: u32) -> impl Future<Output = Result<(), ServerError>>  {
-pub async fn start_ftp_server(path: PathBuf, port: u32) -> Result<(), ServerError>  {
-    let server = libunftp::Server::with_fs(path.clone()).passive_ports(50000..65535);
-    let address = format!("127.0.0.1:{}", port);
-
-    info!("Starting the FTP server at {}", address);
-    server.listen(address.clone()).await
+pub struct FTPServer {
+    server: Server<Filesystem, DefaultUser>,
+    // server: Arc<Server<Filesystem, DefaultUser>>,
+    address: String,
+    // path: PathBuf,
+    shutdown_sender: tokio::sync::broadcast::Sender<()>,
+    // shutdown_receiver: tokio::sync::broadcast::Receiver<()>,
 }
 
-    // pub async fn start(&self) {
-    //     info!("Starting the FTP server at {}", self.address);
-    //     // self.server.listen(self.address.clone()).await.unwrap();
-    // }
+impl FTPServer {
+
+    pub fn new(path: PathBuf, port: u32) -> Self {
+        debug!("Created");
+
+        let (sender, mut receiver) = broadcast::channel(1);
+
+        let server = libunftp::Server::with_fs(path.clone())
+        .passive_ports(50000..65535)
+        .metrics()
+        .shutdown_indicator(async move {
+            receiver.recv().await;
+            info!("Shutting down FTP server");
+            libunftp::options::Shutdown::new().grace_period(Duration::from_secs(11))
+        });
+
+        FTPServer {
+            // server: Arc::new(server),
+            server: server,
+            address: format!("127.0.0.1:{}", port),
+            shutdown_sender: sender,
+        }
+    }
+
+    pub async fn start(&self) {
+        info!("Starting the FTP server at {}", self.address);
+
+        let address_c = self.address.clone();
+
+        // let server_arc = self.server;
+        // let loc = self.clone();
+
+        // tokio::spawn(async move {
+        self.server.listen(address_c).await;
+        debug!("Stopped listening...");
+        // });
+
+        debug!("Done starting...");
+    }
+
+    pub fn stop(&self) {
+        info!("Stopping the FTP server at {}", self.address);
+        self.shutdown_sender.send(());
+    }
+
+}
+
+
+
+// // starts the FTP server as a Tokio task.
+// fn start_ftp(
+//     log: &Logger,
+//     root_log: &Logger,
+//     m: &clap::ArgMatches,
+//     shutdown: tokio::sync::broadcast::Receiver<()>,
+//     done: tokio::sync::mpsc::Sender<()>,
+// ) -> Result<(), String> {
+//     let event_dispatcher =
+//         notify::create_event_dispatcher(Arc::new(log.new(o!("module" => "storage"))), m)?;
+
+//     match m.value_of(args::STORAGE_BACKEND_TYPE) {
+//         None | Some("filesystem") => start_ftp_with_storage(
+//             log,
+//             root_log,
+//             m,
+//             fs_storage_backend(root_log, m),
+//             event_dispatcher,
+//             shutdown,
+//             done,
+//         ),
+//         Some(x) => Err(format!("unknown storage back-end type {}", x)),
+//     }
 // }
