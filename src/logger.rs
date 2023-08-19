f0b34f9e95cdf56087c8a16ab15dfb3edac8e884
João Loureiro <joaofl@gmail.com>
2023-08-20 00:26:47 +0200

    Enable auto-scroll for when the scroll bar is glued at the botton

--- src/logger.rs
+++ src/logger.rs
@@ -8,13 +8,20 @@ impl Log for MyLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
         // Determine whether to enable logging for the given metadata
         // For example, you can enable all levels or only certain levels
-        metadata.level() <= Level::Debug
+        metadata.level() <= Level::Info
     }
 
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
-            let log_prefix = "[FTP]";
-            let _ = self.sender.send(format!("{} {}: {}", log_prefix, record.level(), record.args()));
+            let log_line = format!(
+                "[{}] {}: {}",
+                record.level(),
+                record.target(),
+                record.args()
+            );
+
+            let _ = self.sender.send(log_line.clone());
+            println!("{}", log_line);
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -29,22 +29,30 @@ async fn main() {
     log::set_max_level(LevelFilter::Trace); // Set the maximum log level
 
     let ui = AnyServeUI::new().unwrap();
-    let ui_weak = ui.as_weak();
+    let ui_weak_textedit = ui.as_weak();
+    let ui_weak_ftps = ui.as_weak();
 
     let ftp_server = Arc::new(FTPServer::new());
     let ftp_server_clone = ftp_server.clone();
-
-    let shared_ui = Arc::new(ui);
-    let ui = shared_ui.clone();
-    let ui_clone = shared_ui.clone();
 
     // Get logs printed at the text-box upon a log line print
     slint::spawn_local(async move {
         loop {
             match receiver.recv().await {
                 Ok(text) => {
-                    let t = ui_weak.unwrap().get_te_logs() + &text + "\n";
-                    ui_weak.unwrap().set_te_logs(t);
+
+                    let log_line = format!("{}\n", &text);
+                    let t = ui_weak_textedit.unwrap().get_te_logs() + &log_line;
+                    
+                    // TODO: make sure this "frame-margin" = 38 is true for all the OSs. Maybe calculate it in the begining
+                    let old_pos = (ui_weak_textedit.unwrap().get_te_vh() + 38.0 - ui_weak_textedit.unwrap().get_te_h()) * -1.0;
+                    ui_weak_textedit.unwrap().set_te_logs(t);
+                    let new_pos = (ui_weak_textedit.unwrap().get_te_vh() + 38.0 - ui_weak_textedit.unwrap().get_te_h()) * -1.0;
+                    //Only auto-scroll if already "glued" to the botton 
+                    if ui_weak_textedit.unwrap().get_te_vy() == old_pos {
+                        ui_weak_textedit.unwrap().set_te_vy(new_pos);
+                    }
+
                 }
                 Err(_) => {
                     println!("Something went wrong while receiving log message");
@@ -57,7 +65,7 @@ async fn main() {
 
     ui.on_start_ftp_server(move || {
         // Read and validate the bind address
-        let bind_address = ui_clone.get_le_bind_address().to_string();
+        let bind_address = ui_weak_ftps.unwrap().get_le_bind_address().to_string();
         match utils::validate_ip_port(&bind_address) {
             Ok(()) => debug!("Valid IP:PORT: {:?}", bind_address),
             Err(error) => {
@@ -67,7 +75,7 @@ async fn main() {
         }
 
         // Read and validate the path to be served
-        let path = PathBuf::from(ui_clone.get_le_path().to_string());
+        let path = PathBuf::from(ui_weak_ftps.unwrap().get_le_path().to_string());
         match utils::validate_path(&path) {
             Ok(()) => debug!("Valid path: {:?}", path),
             Err(error) => {
