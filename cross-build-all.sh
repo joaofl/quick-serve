02b3aaf609c36242b3b183d272a157f8bc92652b
João Loureiro <joaofl@gmail.com>
2024-01-08 23:46:13 +0100

    Add compilation script to build all targets

---
+++ cross-build-all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# List of target platforms
+TARGETS=("x86_64-unknown-linux-gnu" "x86_64-pc-windows-gnu" "arm-unknown-linux-gnueabi" "aarch64-unknown-linux-gnu")
+
+mkdir assets
+
+# Cross-compile for each target platform
+for target in "${TARGETS[@]}"; do
+    echo "Cross-compiling for $target..."
+    cross build --release --target "$target" --jobs $(nproc)
+    echo "$target compilation completed."
+
+    cp -v target/${target}/release/quick-serve     assets/quick-serve-${target} 2>/dev/null
+    cp -v target/${target}/release/quick-serve.exe assets/quick-serve.exe-${target} 2>/dev/null
+done
+
+# Clean up
+echo "Cleaning up..."
+cargo clean
