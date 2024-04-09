701cb7cfb1d6530fb363160b2ce20747b6153dd3
João Loureiro <joaofl@gmail.com>
2024-04-09 23:09:57 +0200

    Get default args from existing Protocol crate

--- src/main.rs
+++ src/main.rs
@@ -61,8 +61,8 @@ struct Cli {
     )] verbose: u8,
 
     #[arg(
-        default_missing_value = "8080",
-        help = "Start the HTTP server [default port: 8080]",
+        default_missing_value = Protocol::Http.get_default_port().to_string(),
+        help = format!("Start the HTTP server [default port: {}]", Protocol::Http.get_default_port().to_string()),
         long, required = false, 
         num_args = 0..=1,
         require_equals = true,
@@ -70,8 +70,8 @@ struct Cli {
     )] http: Option<u32>,
 
     #[arg(
-        default_missing_value = "2121",
-        help = "Start the FTP server [default port: 2121]",
+        default_missing_value = Protocol::Ftp.get_default_port().to_string(),
+        help = format!("Start the FTP server [default port: {}]", Protocol::Ftp.get_default_port().to_string()),
         long, required = false, 
         num_args = 0..=1,
         require_equals = true,
@@ -79,8 +79,8 @@ struct Cli {
     )] ftp: Option<u32>,
 
     #[arg(
-        default_missing_value = "6969",
-        help = "Start the TFTP server [default port: 6969]",
+        default_missing_value = Protocol::Tftp.get_default_port().to_string(),
+        help = format!("Start the TFTP server [default port: {}]", Protocol::Tftp.get_default_port().to_string()),
         long, required = false, 
         num_args = 0..=1,
         require_equals = true,
